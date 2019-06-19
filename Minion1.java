3fa26dbea0dc56a65d3eba159abe77e1dc2f4753
abrahampt <maxhacher@hotmail.com>
2019-06-19 10:23:22 -0500

    se creo el primer minion, se le diero atributos de estatura, color y numero de ojos, ademas sabe hablar

---
+++ Minion1.java
@@ -0,0 +1,18 @@
+
+public class Minion1
+{
+    String color="amarillo";
+    String estatura="30";
+    int ojos=1;
+    public String hablar()
+    {
+        return "me want bannana";
+    }
+
+public static void main(String []args)
+{
+    System.out.println("hola mundo");
+    return;
+}
+    
+}
